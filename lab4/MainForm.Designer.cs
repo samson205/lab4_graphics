69879c86a91aa7cfc62a80c6a0a88191f74c454a
samson205 <134064060+samson205@users.noreply.github.com>
2026-04-17 19:31:38 +0300

    Deleted useless button Renamed peaks2 to peaks

--- lab4/MainForm.Designer.cs
+++ lab4/MainForm.Designer.cs
@@ -29,85 +29,53 @@
         private void InitializeComponent()
         {
             pictureBox = new PictureBox();
-            checkBox = new CheckBox();
             button1 = new Button();
-            button2 = new Button();
-            textBox1 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
             SuspendLayout();
             // 
             // pictureBox
             // 
+            pictureBox.BackColor = SystemColors.Control;
             pictureBox.Dock = DockStyle.Top;
             pictureBox.Location = new Point(0, 0);
+            pictureBox.Margin = new Padding(5, 5, 5, 5);
             pictureBox.Name = "pictureBox";
-            pictureBox.Size = new Size(800, 385);
+            pictureBox.Size = new Size(1124, 526);
             pictureBox.TabIndex = 0;
             pictureBox.TabStop = false;
             // 
-            // checkBox
-            // 
-            checkBox.AutoSize = true;
-            checkBox.Location = new Point(369, 409);
-            checkBox.Name = "checkBox";
-            checkBox.Size = new Size(82, 19);
-            checkBox.TabIndex = 1;
-            checkBox.Text = "checkBox1";
-            checkBox.UseVisualStyleBackColor = true;
-            // 
             // button1
             // 
-            button1.Location = new Point(586, 416);
+            button1.Location = new Point(515, 560);
+            button1.Margin = new Padding(5, 5, 5, 5);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(118, 38);
             button1.TabIndex = 2;
-            button1.Text = "button1";
+            button1.Text = "Загрузить";
             button1.UseVisualStyleBackColor = true;
             button1.Click += btnLoad_Click;
             // 
-            // button2
-            // 
-            button2.Location = new Point(699, 415);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 3;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += btnApplyTexture_Click;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(12, 395);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(318, 44);
-            textBox1.TabIndex = 4;
-            // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(button2);
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(1124, 631);
             Controls.Add(button1);
-            Controls.Add(checkBox);
             Controls.Add(pictureBox);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(5, 5, 5, 5);
             MaximizeBox = false;
             Name = "MainForm";
-            Text = "Form1";
+            Text = "ЛР4";
             ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox;
-        private CheckBox checkBox;
         private Button button1;
-        private Button button2;
-        private TextBox textBox1;
     }
 }
--- lab4/MainForm.cs
+++ lab4/MainForm.cs
@@ -4,7 +4,7 @@ namespace lab4
     {
         Bitmap mainBmp;
         Bitmap imgBmp;
-        List<Peak> peaks2 = new List<Peak>();
+        List<Peak> peaks = new List<Peak>();
 
         public MainForm()
         {
@@ -12,10 +12,10 @@ namespace lab4
             mainBmp = new Bitmap(pictureBox.Width, pictureBox.Height);
             pictureBox.Image = mainBmp;
 
-            peaks2.Add(new Peak(50, 300, 0, 1)); // A
-            peaks2.Add(new Peak(50, 50, 0, 0)); // B
-            peaks2.Add(new Peak(300, 50, 1, 0)); // C
-            peaks2.Add(new Peak(300, 300, 1, 1)); // D
+            peaks.Add(new Peak(50, 300, 0, 1)); // A
+            peaks.Add(new Peak(50, 50, 0, 0)); // B
+            peaks.Add(new Peak(300, 50, 1, 0)); // C
+            peaks.Add(new Peak(300, 300, 1, 1)); // D
 
             DrawBorder();
             pictureBox.Invalidate();
@@ -34,22 +34,15 @@ namespace lab4
             }
         }
 
-        private void btnApplyTexture_Click(object sender, EventArgs e)
-        {
-            FillTriangle();
-            DrawBorder();
-            pictureBox.Invalidate();
-        }
-
         private void DrawBorder()
         {
             using (Graphics g = Graphics.FromImage(mainBmp))
             {
                 Pen pen = new Pen(Color.Red);
-                Peak p0 = peaks2[0];
-                Peak p1 = peaks2[1];
-                Peak p2 = peaks2[2];
-                Peak p3 = peaks2[3];
+                Peak p0 = peaks[0];
+                Peak p1 = peaks[1];
+                Peak p2 = peaks[2];
+                Peak p3 = peaks[3];
 
                 g.DrawLine(pen, new Point(p0.X, p0.Y), new Point(p1.X, p1.Y));
                 g.DrawLine(pen, new Point(p1.X, p1.Y), new Point(p2.X, p2.Y));
@@ -60,11 +53,11 @@ namespace lab4
 
         private void FillTriangle()
         {
-            int minX = peaks2.Min(p => p.X);
-            int maxX = peaks2.Max(p => p.X);
+            int minX = peaks.Min(p => p.X);
+            int maxX = peaks.Max(p => p.X);
 
-            int minY = peaks2.Min(p => p.Y);
-            int maxY = peaks2.Max(p => p.Y);
+            int minY = peaks.Min(p => p.Y);
+            int maxY = peaks.Max(p => p.Y);
             for (int y = minY; y <= maxY; y++)
             {
                 for (int x = minX; x <= maxX; x++)
@@ -73,12 +66,12 @@ namespace lab4
                     int isPointIn = IsPointInTriangle(p, out float w1, out float w2, out float w3);
                     if (isPointIn != 0)
                     {
-                        float u = w1 * peaks2[0].U + w2 * peaks2[1].U + w3 * peaks2[2].U;
-                        float v = w1 * peaks2[0].V + w2 * peaks2[1].V + w3 * peaks2[2].V;
+                        float u = w1 * peaks[0].U + w2 * peaks[1].U + w3 * peaks[2].U;
+                        float v = w1 * peaks[0].V + w2 * peaks[1].V + w3 * peaks[2].V;
                         if (isPointIn == 2)
                         {
-                            u = w1 * peaks2[0].U + w2 * peaks2[3].U + w3 * peaks2[2].U;
-                            v = w1 * peaks2[0].V + w2 * peaks2[3].V + w3 * peaks2[2].V;
+                            u = w1 * peaks[0].U + w2 * peaks[3].U + w3 * peaks[2].U;
+                            v = w1 * peaks[0].V + w2 * peaks[3].V + w3 * peaks[2].V;
                         }
 
                         int textureX = (int)(u * (imgBmp.Width - 1));
@@ -92,10 +85,10 @@ namespace lab4
 
         private int IsPointInTriangle(Point p, out float w1, out float w2, out float w3)
         {
-            Point a = new Point(peaks2[0].X, peaks2[0].Y);
-            Point b = new Point(peaks2[1].X, peaks2[1].Y);
-            Point c = new Point(peaks2[2].X, peaks2[2].Y);
-            Point d = new Point(peaks2[3].X, peaks2[3].Y);
+            Point a = new Point(peaks[0].X, peaks[0].Y);
+            Point b = new Point(peaks[1].X, peaks[1].Y);
+            Point c = new Point(peaks[2].X, peaks[2].Y);
+            Point d = new Point(peaks[3].X, peaks[3].Y);
 
             // 2S = (x1 - x3)(y2 - y3) - (x2 - x3)(y1 - y3)
             // ABC
